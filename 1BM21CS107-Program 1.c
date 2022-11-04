c4861c8b64255f33679e2555fd7eaa497a312fd6
MohammadAdnanKhan <98648755+AdnanKhan98@users.noreply.github.com>
2022-11-04 03:31:51 -0700

    Update 1BM21CS107-Program 1.c

--- 1BM21CS107-Program 1.c
+++ 1BM21CS107-Program 1.c
@@ -1,65 +1,61 @@
-#include <stdio.h>
-#include<stdlib.h>
-#define MAX 4
-int a[MAX];
-int top = -1;
-void push(int data)
+#include<stdio.h>
+#include<stdio.h>
+#define Stack_size 5
+int st[5], top = -1, item;
+
+void push()
 {
-    if(top==MAX-1)
-    {
-        printf("Stack overflow\n");
-        return;
-    }
-    
-    top=top+1;
-    a[top]=data;
+	if(top==Stack_size - 1)
+		printf("Stack Overflow \n");
+	else
+	{
+		top++;
+		st[top] = item;
+	}
 }
 
 int pop()
 {
-    int value;
-    if(top<0)
-    {
-        printf("Stack underflow\n");
-      
-    }
-    value=a[top];
-    top=top-1;
-    return value;
-   
-}
-void display()
-{
-    int i;
-    if(top==-1)
-    {
-        printf("Stack is empty\n");
-    }
-    for(i=0;i<top;i++)
-    printf("%d",a[i]);
+	int delitem;
+		if(top==-1)
+			printf("Stack Underflow \n");
+		else
+		{
+			delitem = st[top];
+			top--;
+			printf("The element deleted is %d \n",delitem);
+			return delitem;
+		}
 }
 
-void main()
+void display()
 {
+	int i;
+	if(top == -1)
+		printf("Stack is empty\n");
+    for(i=0; i <=top;i++)
+        printf("%d \n", st[i]);
+}
+
+int main()
+{   int n,r;
     while(1)
-   {
-   int n,data;
-   printf("Enter the operation:1.PUSH   2.POP   3.DISPLAY ");
-   scanf("%d",&n);
-   
-       switch(n)
-       {
-           case 1: printf("enter the data ");
-           scanf("%d",&data);
-           push(data);
-           break;
-           case 2:data=pop();
-           printf("%d\n",data);
-           break;
-           case 3:display();
-           break;
-           default:exit(0);
-           
-       }
-   }
+    {
+        printf("Enter the option you want to choose \n 1. To insert a element \n 2. To remove an element \n 3. To display the elements \n 4. Exit\n ");
+        scanf("%d",&n);
+        switch(n)
+        {
+            case 1:printf("Enter the element you want to add\n");
+                   scanf("%d",&r);
+                   item = r;
+                    push();
+                    break;
+            case 2:pop();
+                    break;
+            case 3: display();
+                    break;
+            default:exit(0);
+                    break;
+        }
+    }
 }
